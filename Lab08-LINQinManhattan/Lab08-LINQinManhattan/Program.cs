027475fbb027563c2025a65416b47ad7e8cfe4bc
Christina Gislason <christinamkcho@gmail.com>
2018-10-21 23:20:33 -0700

    added json file with manhattan data

---
+++ Lab08-LINQinManhattan/Lab08-LINQinManhattan/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Newtonsoft.Json;
+
+namespace Lab08_LINQinManhattan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
