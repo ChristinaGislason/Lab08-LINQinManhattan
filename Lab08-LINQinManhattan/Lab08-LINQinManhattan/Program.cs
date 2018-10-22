58ee521b53c0e50c1c995490df38587f8fd0d8bc
Christina Gislason <christinamkcho@gmail.com>
2018-10-22 00:14:41 -0700

    initial Json conversion code; errors

--- Lab08-LINQinManhattan/Lab08-LINQinManhattan/Program.cs
+++ Lab08-LINQinManhattan/Lab08-LINQinManhattan/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Lab08_LINQinManhattan.Classes;
 using Newtonsoft.Json;
 
 namespace Lab08_LINQinManhattan
@@ -8,6 +12,34 @@ namespace Lab08_LINQinManhattan
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            JsonConversion();
+        }
+
+        static void JsonConversion()
+        {
+            string path = "../../../data.json";
+            string text = "";
+
+            // using streamreader to access json file
+            using (StreamReader sr = File.OpenText(path))
+            {
+                // get all the text
+                text = sr.ReadToEnd();
+            }
+
+            // deserialize the JSON to convert to the Neighborhoods object
+            List<Neighborhoods> myNeighborhoods = JsonConvert.DeserializeObject<List<Neighborhoods>>(text);
+
+            // query to get all neighborhoods in manhattan  
+            var neighborhoodNames = myNeighborhoods.Where(name => name.Neighborhood == "Neighborhood");
+
+            foreach (var item in neighborhoodNames)
+            {
+                if (item.Neighborhood == "Neighborhood")
+                {
+                    Console.WriteLine(item.Neighborhood);
+                }
+            }
         }
     }
 }
